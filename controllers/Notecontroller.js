430b23891f0cb176f2b79851fd0b5963ea444042
Carlos Montiel <eltoro.montiel@gmail.com>
2023-08-30 11:38:01 -0500

    the method to colsult all notes was added, now working on consulting one note

--- controllers/Notecontroller.js
+++ controllers/Notecontroller.js
@@ -1,8 +1,25 @@
-import  { PrismaClient } from '@prisma/client'
 
+import  { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-export const getAllNotes = async () => {
-    const allnotes = await prisma.note.findMany()
+
+export const getAllNotes = async (req, res) => {
+    try{
+        const allnotes = await prisma.note.findMany()
+        res.json(allnotes)
+    }catch(error){
+        res.json(error.message)
+    }
+    
+}
+
+export const getOneNote = async (req, res) => {
+    const idToConsult = req.params.id
+    try{
+        const note = await prisma.note.findFirst(idToConsult)
+        res.json(note)
+    }catch(error){
+
+    }
     
 }
--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express  from 'express'
 import cors from 'cors'
 import  'dotenv/config'
 
-import NoteRouter from './routes/NoteRoutes.js'
-import CategoryRouter from './routes/CategoryRoutes.js'
+//import CategoryRouter from './routes/CategoryRoutes.js'
+import NoteRoutes from './routes/NoteRoutes.js'
 
 const PORT = process.env.PORT || 8000
 
@@ -11,7 +11,7 @@ const App = express()
 
 App.use(cors())
 App.use(express.json())
-App.use('/notes', NoteRouter)
-App.use('/categories', CategoryRouter)
+App.use('/notes', NoteRoutes)
+//App.use('/categories', CategoryRouter)
 
 App.listen( PORT, () => {console.log('Aplication is listening on port ' + PORT)})
